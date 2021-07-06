62210110b3d6b23611e42e84a283acaba8b92a02
Joey Gold <joey.gold@softwire.com>
2021-07-06 11:24:26 +0100

    Put inside of FOR loop into function to look at debugging features of Visual Studio Code.

--- index.ts
+++ index.ts
@@ -1,15 +1,20 @@
-var maxNum=100;
+var maxNum : number; 
+maxNum = 100;
 for (let i=1;i<=maxNum;i++){
-    //Create an empty string that can have Fizz and/or Buzz appended to it (or be changed to a number).
-    let toPrint = "";
-    if (i%3 == 0) {
-        toPrint += "Fizz";
-    }
-    if (i%5 == 0) {
-        toPrint += "Buzz";
-    }
-    if (toPrint==""){
-        toPrint = i.toString();
-    }
-    console.log(toPrint);
+    printFizzBuzz(i);
+}
+
+function printFizzBuzz(i){
+        //Create an empty string that can have Fizz and/or Buzz appended to it (or be changed to a number).
+        let toPrint = "";
+        if (i%3 == 0) {
+            toPrint += "Fizz";
+        }
+        if (i%5 == 0) {
+            toPrint += "Buzz";
+        }
+        if (toPrint==""){
+            toPrint = i.toString();
+        }
+        console.log(toPrint);
 }
